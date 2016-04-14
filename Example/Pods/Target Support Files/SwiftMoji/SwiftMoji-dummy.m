50113377c87b3d41f1a4353d4ea8d1bc214d8301
koichi <tanakakoichi256@gmail.com>
2016-04-14 23:22:36 +0900

    Add Pods to git repository

---
+++ Example/Pods/Target Support Files/SwiftMoji/SwiftMoji-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SwiftMoji : NSObject
+@end
+@implementation PodsDummy_SwiftMoji
+@end
---
+++ Example/Pods/Target Support Files/SwiftMoji/SwiftMoji-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SwiftMojiVersionNumber;
+FOUNDATION_EXPORT const unsigned char SwiftMojiVersionString[];
+
