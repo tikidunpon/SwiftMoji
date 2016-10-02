0ff4ec8ee12d70f650d8bc15c84a5cddd530aaed
koichi <tanakakouichi@MacBook-2.local>
2016-10-03 00:10:39 +0900

    import UIKit

--- Pod/Classes/SwiftMoji.swift
+++ Pod/Classes/SwiftMoji.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 private class MojiConstant {
     static let ASCII_UPPERCASE_SET = Set(["A", "B", "C", "D", "E", "F", "G","H", "I", "J", "K", "L", "M", "N","O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"])
